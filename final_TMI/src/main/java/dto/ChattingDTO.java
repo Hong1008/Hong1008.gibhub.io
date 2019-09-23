2d942ad605271ac91f81dced287d2a08751c1b57
j1su94 <j1su_94@hanmail.net>
2019-09-23 19:48:06 +0900

    박기현 9월 23일

--- final_TMI/src/main/java/dto/ChattingDTO.java
+++ final_TMI/src/main/java/dto/ChattingDTO.java
@@ -8,50 +8,61 @@ public class ChattingDTO {
 	private String pro_id;
 	private String chat_id;
 	private String id;
-
 	private String chat_content;
 	private String chat_time;
 	private String chat_date;
-
+	private String realfilename;
 	private List<MultipartFile> filename;
-	private  List<String> upload;
+	private List<String> upload;
 
 	public ChattingDTO() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public String getPro_id() {
 		return pro_id;
 	}
+
 	public void setPro_id(String pro_id) {
 		this.pro_id = pro_id;
 	}
+
 	public String getChat_id() {
 		return chat_id;
 	}
+
 	public void setChat_id(String chat_id) {
 		this.chat_id = chat_id;
 	}
+
 	public String getChat_content() {
 		return chat_content;
 	}
+
 	public void setChat_content(String chat_content) {
 		this.chat_content = chat_content;
 	}
+
 	public String getChat_time() {
 		return chat_time;
 	}
+
 	public void setChat_time(String chat_time) {
 		this.chat_time = chat_time;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getChat_date() {
 		return chat_date;
 	}
+
 	public void setChat_date(String chat_date) {
 		this.chat_date = chat_date;
 	}
@@ -59,14 +70,25 @@ public class ChattingDTO {
 	public List<MultipartFile> getFilename() {
 		return filename;
 	}
+
 	public void setFilename(List<MultipartFile> filename) {
 		this.filename = filename;
 	}
+
 	public List<String> getUpload() {
 		return upload;
 	}
+
 	public void setUpload(List<String> upload) {
 		this.upload = upload;
 	}
 
+	public String getRealfilename() {
+		return realfilename;
+	}
+
+	public void setRealfilename(String realfilename) {
+		this.realfilename = realfilename;
+	}
+	
 }
